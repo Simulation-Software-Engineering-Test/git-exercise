4bc83996adc434e571a6a2f8181355adccad2be4
Felix <st157773@stud.uni-stuttgart.de>
2024-10-23 17:27:27 +0200

    Task B: added optional arg tolerance to `contains`
    - also fixed a small mistake with the `is_in_interval` method

--- exercise3.py
+++ exercise3.py
@@ -25,13 +25,14 @@ class Rectangle:
     def upper_right(self) -> Point2D:
         return self.corner(3)
 
-    # def contains(self, point: Point2D, tolerance: float = 0.0) -> bool: # Task B
-    def contains(self, point: Point2D) -> bool:
+    def contains(self, point: Point2D, tolerance: float = 0.0) -> bool: # Task B
         # Task A: remove duplication by defining a function
         #         that checks if a value is within an interval
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
+        return self.is_in_interval(ll_px, 0.0 - tolerance, self._dx + tolerance) \
+            and self.is_in_interval(ll_py, 0.0 - tolerance, self._dy + tolerance)
         return ll_px >= 0 and ll_px <= self._dx \
             and ll_py >= 0 and ll_py <= self._dy
 
@@ -41,7 +42,7 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    def is_in_interval(i:float, start:float, end:float) -> bool: # Task A
+    def is_in_interval(self, i: float, start: float, end: float) -> bool: # Task A
         return i >= start and i <= end
 
 
