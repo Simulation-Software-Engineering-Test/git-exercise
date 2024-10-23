424db430bbac7bb5b020d28f23dafb2fc5357e81
Marcel Wolkober <44367835+MarcelWolkober@users.noreply.github.com>
2024-10-23 17:19:31 +0200

    Implement is_in_interval function (#6)
    Implement the is_in_interval function in exercise3.py.

--- exercise3.py
+++ exercise3.py
@@ -41,7 +41,8 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    # def is_in_interval(...) -> bool: # Task A
+    def is_in_interval(i:float, start:float, end:float) -> bool: # Task A
+        return i >= start and i <= end
 
 
 def test_rectangle_contains_exact() -> None:
